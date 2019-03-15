6bbcddc6987a452d7f5af83f2a920e6ddacd6742
silent <1183611386@qq.com>
2019-03-16 00:20:38 +0800

    重要的事情说三遍

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("老大好帅！！！");
+        System.out.println("老大好帅！！！");
+        System.out.println("老大好帅！！！");
+        System.out.println("重要的事情说三遍！！！");
     }
 }
