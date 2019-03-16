29692181eb14f83f0bd10b0cf4ba7cc71a2cb14c
silent <1183611386@qq.com>
2019-03-16 11:42:00 +0800

    4

--- src/Main.java
+++ src/Main.java
@@ -11,23 +11,29 @@ public class Main {
         for(int i =0;i<n;i++){
             nums[i]=scan.nextInt();
         }
-        Arrays.sort(nums);
+        double[] seg = new double[n*m];
         double res =0;
+        int idx =0;
         for(int i =n-1;i>=0;i--){
             for(int j=1;j<=m;j++){
                 double segement = nums[i]/(j+0.00);
                 DecimalFormat df = new DecimalFormat("#.00");
                 segement = Double.parseDouble(df.format(segement));
-                int maxLen =0;
-                for (int k=0;k<n;k++){
-                    maxLen += nums[k]/segement;
-                }
-                if(maxLen >=m){
-                    res = Math.max(res,segement);
-                    break;
-                }
+                seg[idx] = segement;
+                idx++;
             }
+        }
 
+        Arrays.sort(seg);
+        for(int i=seg.length-1;i>=0;i--){
+            int maxLen =0;
+            for (int k=0;k<n;k++){
+                maxLen += nums[k]/seg[i];
+            }
+            if(maxLen >=m){
+                res = seg[i];
+                break;
+            }
         }
         System.out.println(String.format("%.2f",res));
     }
