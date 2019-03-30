2dcaa07150c012181bc4dbad4aff913d27bc7a2e
silent <1183611386@qq.com>
2019-03-30 20:35:20 +0800

    dierti

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,45 @@
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+class TreeNode{
+    TreeNode left;
+    TreeNode right;
+    int val;
+    TreeNode(int x){
+        this.val=x;
+    }
 
+}
 public class Main {
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int res =0;
-        while(num!=0){
-            if((num&1)==1){
-                res++;
-            }
-            num=num>>1;
+        String input = scanner.nextLine();
+        String[] str= input.split(",");
+        if( str.length<=1 ){
+            System.out.println("True");
+            return;
         }
-        System.out.println(res);
+        int[] nums = new int[str.length];
+        for(int i=0;i<str.length;i++){
+            nums[i] = Integer.valueOf(str[i]);
+        }
+        boolean isBST = isBST(nums);
+        System.out.println(isBST ? "True":"False");
     }
+
+    private static boolean isBST(int[] nums) {
+        return isBST(nums,0,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+
+    private static boolean isBST(int[] nums, int i, int min, int max) {
+        if(i>(nums.length/2)-1){
+            return true;
+        }
+        if(nums[i]<=min || nums[i]>=max){
+            return false;
+        }
+        return isBST(nums,i*2+1,min,nums[i])&&isBST(nums, i*2+2, nums[i], max);
+    }
+
 
 }
