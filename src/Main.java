9d8ec86ff8fe5de4281cd51890cca25507667f44
silent <1183611386@qq.com>
2019-03-30 20:41:32 +0800

    dierti2

--- src/Main.java
+++ src/Main.java
@@ -14,32 +14,38 @@ public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        String[] str= input.split(",");
-        if( str.length<=1 ){
+        String[] nums= input.split(",");
+        if( nums.length<=1 ){
             System.out.println("True");
             return;
         }
-        int[] nums = new int[str.length];
-        for(int i=0;i<str.length;i++){
-            nums[i] = Integer.valueOf(str[i]);
+        TreeNode root = new TreeNode(Integer.valueOf(nums[0]));
+        Deque<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        for(int i =1;i+1<nums.length;i=i+2){
+            TreeNode now = queue.pop();
+            TreeNode left = new TreeNode(Integer.valueOf(nums[i]));
+            TreeNode right = new TreeNode(Integer.valueOf(nums[i+1]));
+            now.left=left;
+            now.right=right;
+            queue.add(left);
+            queue.add(right);
         }
-        boolean isBST = isBST(nums);
+        boolean isBST = isBST(root);
         System.out.println(isBST ? "True":"False");
     }
 
-    private static boolean isBST(int[] nums) {
-        return isBST(nums,0,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    private static boolean isBST(TreeNode root) {
+        return isBST(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
     }
 
-    private static boolean isBST(int[] nums, int i, int min, int max) {
-        if(i>(nums.length/2)-1){
+    private static boolean isBST(TreeNode root, int min, int max) {
+        if(root==null){
             return true;
         }
-        if(nums[i]<=min || nums[i]>=max){
+        if(root.val<=min || root.val >=max){
             return false;
         }
-        return isBST(nums,i*2+1,min,nums[i])&&isBST(nums, i*2+2, nums[i], max);
+        return isBST(root.left,min,root.val) && isBST(root.right,root.val,max);
     }
-
-
 }
