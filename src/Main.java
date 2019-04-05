764b2d2227c519ad96bd9f4c45c9a6c402ff4f45
silent <1183611386@qq.com>
2019-04-05 20:36:17 +0800

    dierti

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,21 @@ public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        long[] w = new long[n];
-        int[] money = new int[n];
-        for(int i =0;i<n;i++){
-            w[i]= scanner.nextLong();
-        }
-        for(int i =0;i<n;i++){
-            money[i]= scanner.nextInt();
-        }
-
-        int[] dp = new int[n];
-        dp[0] = money[0];
-        int wuli =w[0];
-        for(int i =1;i<n;i++){
-            if(w[i]<wuli){
-                dp[i] = dp[i-1];
+        scanner.nextLine();
+        String s = scanner.nextLine();
+        Stack<Character> stack = new Stack<Character>();
+        char[] c= s.toCharArray();
+        for(int i=0;i<c.length;i++){
+            if(stack.isEmpty()){
+                stack.push(c[i]);
             }else{
-                dp[i] = dp[i-1] + money[i];
-                wuli+=w[i];
+                if(stack.peek()!=c[i] ){
+                    stack.pop();
+                }else{
+                    stack.push(c[i]);
+                }
             }
         }
-        System.out.println(dp[n-1]);
+        System.out.println(stack.size());
     }
 }
