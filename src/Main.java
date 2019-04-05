81c09835ce097361fe96ecac056f5dd07cfecb22
silent <1183611386@qq.com>
2019-04-05 20:23:51 +0800

    disanti

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,28 @@
-import java.text.DecimalFormat;
 import java.util.*;
-class TreeNode{
-    TreeNode left;
-    TreeNode right;
-    int val;
-    TreeNode(int x){
-        this.val=x;
-    }
-
-}
 public class Main {
-
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] nums= input.split(",");
-        if( nums.length<=1 ){
-            System.out.println("True");
-            return;
+        int n = scanner.nextInt();
+        long[] w = new long[n];
+        int[] money = new int[n];
+        for(int i =0;i<n;i++){
+            w[i]= scanner.nextLong();
         }
-        TreeNode root = new TreeNode(Integer.valueOf(nums[0]));
-        Deque<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        for(int i =1;i+1<nums.length;i=i+2){
-            TreeNode now = queue.pop();
-            TreeNode left = new TreeNode(Integer.valueOf(nums[i]));
-            TreeNode right = new TreeNode(Integer.valueOf(nums[i+1]));
-            now.left=left;
-            now.right=right;
-            queue.add(left);
-            queue.add(right);
+        for(int i =0;i<n;i++){
+            money[i]= scanner.nextInt();
         }
-        boolean isBST = isBST(root);
-        System.out.println(isBST ? "True":"False");
-    }
 
-    private static boolean isBST(TreeNode root) {
-        return isBST(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
-    }
-
-    private static boolean isBST(TreeNode root, int min, int max) {
-        if(root==null){
-            return true;
+        int[] dp = new int[n];
+        dp[0] = money[0];
+        int wuli =w[0];
+        for(int i =1;i<n;i++){
+            if(w[i]<wuli){
+                dp[i] = dp[i-1];
+            }else{
+                dp[i] = dp[i-1] + money[i];
+                wuli+=w[i];
+            }
         }
-        if(root.val<=min || root.val >=max){
-            return false;
-        }
-        return isBST(root.left,min,root.val) && isBST(root.right,root.val,max);
+        System.out.println(dp[n-1]);
     }
 }
