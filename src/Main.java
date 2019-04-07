6163ed43d3dfb2fb7fed243977ec08822b543128
silent <1183611386@qq.com>
2019-04-07 20:41:26 +0800

    dierti_nihy

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,37 @@ public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        scanner.nextLine();
-        String s = scanner.nextLine();
-        Stack<Character> stack = new Stack<Character>();
-        char[] c= s.toCharArray();
-        for(int i=0;i<c.length;i++){
-            if(stack.isEmpty()){
-                stack.push(c[i]);
-            }else{
-                if(stack.peek()!=c[i] ){
-                    stack.pop();
-                }else{
-                    stack.push(c[i]);
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]= scanner.nextInt();
+        }
+        int ans =0;
+        int idx =0;
+        for(int i=0;i<n;){
+            if(nums[i]>0){
+                // 找到第一个小于0的
+                for (;idx<n;){
+                    if(nums[idx]<0){
+                        boolean full = (-nums[idx])>= nums[i];
+                        if(full){
+                            ans+=nums[i]*Math.abs(idx-i);
+                            nums[idx]+=nums[i];
+                            nums[i] =0;
+                        }else{
+                            ans+= (-nums[idx]) *Math.abs(idx-i);
+                            nums[i] += nums[idx];
+                            nums[idx] =0;
+
+                        }
+                        break;
+                    }else{
+                        idx++;
+                    }
                 }
+            }else{
+                i++;
             }
         }
-        System.out.println(stack.size());
+        System.out.println(ans);
     }
 }
