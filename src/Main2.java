a350916692d068eda870099ee9202266d2998294
silent <1183611386@qq.com>
2019-04-13 20:08:45 +0800

    2

---
+++ src/Main2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main2 {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val){
+            this.val = val;
+        }
+    }
+    public static void main(String...args) throws Exception{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        int n = Integer.parseInt(br.readLine());
+        TreeNode root = new TreeNode(1);
+
+    }
+}
